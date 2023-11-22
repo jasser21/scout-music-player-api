4b82b13548b72431e5e36b22e325dba620dfec1a
jasser21 <jesuisjasser@gmail.com>
2023-11-22 12:09:43 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
+source ../bin/activate
+python manage.py makemigrations
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py runserver
