628ee9d21643d15841dcc070b7358f9a92fb44e9
jasser21 <jesuisjasser@gmail.com>
2023-11-22 12:13:22 +0100

    2nd

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-pip install -m requirements.txt
+pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py collectstatic --no-input
 python manage.py migrate
